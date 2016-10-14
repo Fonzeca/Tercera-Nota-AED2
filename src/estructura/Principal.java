ec1fc8dc8fb072c95f938e461da89ad5df0f3d34
AlexisFonzo <alexisfonzos_@hotmail.com>
2016-10-14 03:23:35 -0300

    Arreglando main

--- src/estructura/Principal.java
+++ src/estructura/Principal.java
@@ -1,28 +1,26 @@
 package estructura;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.RandomAccessFile;
+import visual.Ventana;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		String aux = new File("src/recursos/cod.txt").getAbsolutePath();
+//		String aux = new File("src/recursos/cod.txt").getAbsolutePath();
+//		
+//		LectorArchivo lector = new LectorArchivo(aux);
+//		lector.leerArchivo();
+//		Lista lis = lector.getLista();
+//		ArbolHuffman ah = new ArbolHuffman(lis);
+//		TablaHuffman th = new TablaHuffman(ah);
+//		try {
+//			Codificador cod = new Codificador(new RandomAccessFile(aux, "rw"), aux, th);
+//			cod.codificar();
+//		} catch (FileNotFoundException e) {
+//			System.out.println("SHE LOCO");
+//			e.printStackTrace();
+//		}
 		
-		LectorArchivo lector = new LectorArchivo(aux);
-		lector.leerArchivo();
-		Lista lis = lector.getLista();
-		ArbolHuffman ah = new ArbolHuffman(lis);
-		TablaHuffman th = new TablaHuffman(ah);
-		try {
-			Codificador cod = new Codificador(new RandomAccessFile(aux, "rw"), aux, th);
-			cod.codificar();
-		} catch (FileNotFoundException e) {
-			System.out.println("SHE LOCO");
-			e.printStackTrace();
-		}
-		
-		
+		new Ventana();
 		
 	}
 
